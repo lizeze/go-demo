b1cccaa706aec3dc0457f7078548cdb7247a9767
zeze.li <lizeze.boy@gmail.com>
2021-07-01 22:25:10 +0800

    first commit

---
+++ src/constant.go
@@ -0,0 +1,22 @@
+package main
+
+ //常熟
+import (
+    "fmt"
+    "math"
+)
+
+const s string = "constant"
+
+func main() {
+    fmt.Println(s)
+
+    const n = 500000000
+
+    const d = 3e20 / n
+    fmt.Println(d)
+
+    fmt.Println(int64(d))
+
+    fmt.Println(math.Sin(n))
+}
